4bcd0df27fb7c61bd6afbb38d68cde692c4a8f06
thieka sow <sowthieka123@gmail.com>
2025-03-22 13:50:23 +0000

    Modification

--- app/server.py
+++ app/server.py
@@ -5,7 +5,7 @@ class SimpleHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        message = "<html><body><h1>Hello, Thieka!</h1></body></html>"
+        message = "<html><body><h1>Hello, Thieka Sow! Bienvenue</h1></body></html>"
         self.wfile.write(message.encode("utf-8"))
 
 
